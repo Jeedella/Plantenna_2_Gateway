2735647789d598ed2d33ae4ce8b3587595482366
kee3p0 <47350551+kee3p0@users.noreply.github.com>
2022-01-28 13:17:25 +0100

    Fixed BluetoothDatabaseLink.py
    Fixed errors due to json dictionary using different dictionary names.

--- local_storage/BluetoothDatabaseLink.py
+++ local_storage/BluetoothDatabaseLink.py
@@ -6,8 +6,8 @@ import time
 import local_database
 
 class Communication:
-    def __init__(self, id, time, temperature, humidity, pressure, battery, airflow):
-        self.id = id
+    def __init__(self, time, temperature, humidity, pressure, battery, airflow):
+        self.id = 0
         self.time = time
         self.temperature = temperature
         self.humidity = humidity
@@ -18,13 +18,14 @@ class Communication:
 def Init(): # Initialise UART link
     serialCom = serial.Serial('/dev/ttyACM0', 115200, timeout = 1) #change '/dev/ttyACM0' to name for nrf
     serialCom.reset_input_buffer()
+    return serialCom
 
 def GetData(): # Get transmitted data and convert into dictionary
     transmitedData = serialCom.readline().decode('utf-8').rstrip()
     return transmitedData
 
 def SendData(message):
-    serialCom.write(message + "\n")
+    serialCom.write(str.encode(message + "\n"))
 
 def ConvertJson(data):
     convertedVal = json.loads(data) # Dictionary conversion
@@ -33,23 +34,26 @@ def ConvertJson(data):
 def StoreData(transmitedData): # Get dictionary of gata and tramsfer it to database 
     
     if bool(transmitedData):
-        latestData = Communication(transmitedData["id"], transmitedData["time"], transmitedData["temperature"], transmitedData["humidity"], transmitedData["pressure"], transmitedData["battery"], transmitedData["airflow"]) # Get dictionary data and save it into class=
+        latestData = Communication(transmitedData["time"], transmitedData["temp"], transmitedData["humi"], transmitedData["pres"], transmitedData["batt"], transmitedData["airf"]) # Get dictionary data and save it into class=
         local_database.insert_data(latestData.temperature, latestData.humidity, latestData.battery, latestData.airflow, latestData.pressure, latestData.id)
         SendData("dn") # acknowledgement that data was received correctly
 
 if __name__ == '__main__':
-    Init()
+    serialCom = Init()
+    
 
     while True:
 
         if (serialCom.in_waiting > 0):
             data = GetData()
+            print(data)
 
             if (data == "nd"):
                 SendData("ok")
-            else:    
+                print("ask for data\n")
+            elif(data[:1] == "{"):    
+                print("sending data\n")
                 jsonData = ConvertJson(data)
                 StoreData(jsonData)
         else:
-            time.sleep(5) # Sleeps if there is no data to receive
-                
+            time.sleep(5) # Sleeps 30s if there is no data to receive
